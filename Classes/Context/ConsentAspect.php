c630b8f109e7562aae05c17c30157534e6cd07a6
ffassing <ffassing@mediengarage.de>
2022-04-07 09:40:55 +0200

    moved consents available into typoscript

--- Classes/Context/ConsentAspect.php
+++ Classes/Context/ConsentAspect.php
@@ -4,17 +4,15 @@ namespace MEDIENGARAGE\Piwikconsentmanager\Context;
 
 use TYPO3\CMS\Core\Context\AspectInterface;
 use TYPO3\CMS\Core\Context\Exception\AspectPropertyNotFoundException;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 
 /**
  * The aspect contains information about a visitor's data privacy consents
  */
 class ConsentAspect implements AspectInterface
 {
-    private $consentsAvailable = [
-        'custom_consent',
-        'analytics'
-    ];
-
     /**
      * @var array<bool>
      */
@@ -25,7 +23,15 @@ class ConsentAspect implements AspectInterface
      */
     public function __construct(array $consents = [])
     {
-        foreach ($this->consentsAvailable as $consent) {
+        $configurationManager = GeneralUtility::makeInstance(ConfigurationManager::class);
+        $settings = $configurationManager->getConfiguration(
+            ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS,
+            'piwikconsentmanager'
+        );
+
+        $consentsAvailable = GeneralUtility::trimExplode(',', $settings['consentsAvailable']);
+
+        foreach ($consentsAvailable as $consent) {
             // TODO: Instead of not equals -1 better equals 1 or whatever positiv value is
             $this->consents[$consent] = !empty($consents[$consent]) && $consents[$consent]['status'] !== -1;
         }
