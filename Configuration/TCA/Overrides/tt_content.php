87c041abd17308c4b972e3ab56d1ea742f984dff
cfassing <cfassing@mediengarage.de>
2022-05-04 17:05:11 +0200

    [TASK] Verarbeitung Inhaltselemente

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -12,6 +12,20 @@
     'after'
 );
 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'LLL:EXT:piwik_consent_manager/Resources/Private/Language/locallang.xlf:piwikconsentmanager_generic',
+        'piwikconsentmanager_generic',
+        'content-image',
+    ],
+    'textmedia',
+    'after'
+);
+
+
+
 $GLOBALS['TCA']['tt_content']['types']['piwikconsentmanager_youtube'] = [
     'columnsOverrides' => [
         'list_type' => [
@@ -42,4 +56,38 @@ $GLOBALS['TCA']['tt_content']['types']['piwikconsentmanager_youtube'] = [
             rowDescription,
         --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
     '
+];
+
+
+$GLOBALS['TCA']['tt_content']['types']['piwikconsentmanager_generic'] = [
+    'columnsOverrides' => [
+        'list_type' => [
+            'label' => 'LLL:EXT:piwik_consent_manager/Resources/Private/Language/locallang.xlf:consent_type',
+            'config' => [
+                'itemsProcFunc' => MEDIENGARAGE\Piwikconsentmanager\Utility\ConsentSelectItems::class . '->getConsentSelectItems',
+                'default' => 'custom_consent'
+            ]
+        ],
+        'image' => [
+            'label' => 'LLL:EXT:piwik_consent_manager/Resources/Private/Language/locallang.xlf:preview_image',
+        ]
+    ],
+    'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+        --palette--;;general,
+            list_type,
+            image,
+            header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header.ALT.shortcut_formlabel,
+            records;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:records_formlabel,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+            categories,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+	'
 ];
