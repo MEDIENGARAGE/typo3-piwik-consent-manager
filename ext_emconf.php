92e9e7b00d56c9d8f96bd23c558e749106634d18
cfassing <cfassing@mediengarage.de>
2022-06-28 11:23:30 +0200

    [TASK] set version and dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'ffassing@mediengarage.de',
     'state' => 'alpha',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
