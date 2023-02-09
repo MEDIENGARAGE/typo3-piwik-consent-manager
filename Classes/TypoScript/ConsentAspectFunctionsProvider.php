9f96808c284efd8637af695a1ebec170c5b3c304
Claus Fassing <cfassing@mediengarage.de>
2023-02-09 17:03:55 +0100

    [!!!][TASK] Extend / reworking custom expression

--- Classes/TypoScript/ConsentAspectFunctionsProvider.php
+++ Classes/TypoScript/ConsentAspectFunctionsProvider.php
@@ -19,18 +19,20 @@ class ConsentAspectFunctionsProvider implements ExpressionFunctionProviderInterf
 
     /**
      * Allowed consent
-     * Example [ConsentAspect('custom_consent')]
+     * [ConsentAspect('PIWIK_TAG_NAME','PIWIK_KEY')]
+     * Example [ConsentAspect('custom_consent','f93b6a45-0815-1337-0815-1337a19a0995')]
      * @return ExpressionFunction
      */
     protected function getConsentAspect(): ExpressionFunction
     {
         return new ExpressionFunction('ConsentAspect', function () {
             // Not implemented, we only use the evaluator
-        }, function (array $arguments, string $consentType) {
-            $extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class)
-                ->get('piwik_consent_manager');
-            $cookieName = 'ppms_privacy_' . $extensionConfiguration[ConsentManagerController::$CM_KEY];
+        }, function (array $arguments, string $consentType, string $key) {
+            $cookieName = 'ppms_privacy_' . $key;
             $cookies = $arguments['request']->getCookieParams();
+            if(empty($cookies)) {
+                return false;
+            }
             if (array_key_exists($cookieName, $cookies)) {
                 $cookieData = json_decode($cookies[$cookieName], true);
                 if ($cookieData['consents'][$consentType]['status'] === 1) {
