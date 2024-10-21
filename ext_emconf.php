18386d404d1263abac7dda893c4b22a3cd370d12
Claus Fassing <cfassing@mediengarage.de>
2024-10-21 12:03:15 +0200

    Raise version to 0.2.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Florian Fassing',
     'author_email' => 'ffassing@mediengarage.de',
     'state' => 'beta',
-    'version' => '0.2.4',
+    'version' => '0.2.5',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
